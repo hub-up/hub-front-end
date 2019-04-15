0375c2519fb8df15bd5ebc0c89bfe2a496c74d7e
Joseph <z@charmed.tech>
2019-04-15 16:26:05 -0700

    Update room, leave, details lines

--- instructions.js
+++ instructions.js
@@ -3,19 +3,19 @@
 // Colors
 const chalk = require('chalk');
 
-const instructions = `        
+const todo = chalk.yellow('TODO');
+
+const instructions = `
 ${chalk.underline.bold('Chat Commands')}
 /bye ← Disconnect from the server and exit the program
-/details ← See your name, the room you're in, and a list of other users in your current room
+/details ← ${todo}: Needs update: See your name, the room you're in, and a list of other users in your current room
 /help ← This menu
-/leave  ← TODO: Leave the current room and return to the lobby
-/join ${chalk.blue('room')} ← TODO: Join the ongoing chat in ${chalk.blue('room')}
-/me :D ← ${chalk.magenta(':D')}
+/leave  ← Leave the current room and return to the lobby
+/join ${chalk.blue('room')} ← ${todo}: Join the ongoing chat in ${chalk.blue('room')}
+/me :D ← ${todo}: Should be hooked to emojic. ${chalk.magenta(':D')}
 /msg ${chalk.yellow('user')} ← Send a direct message to ${chalk.yellow('user')}
 /nick ${chalk.cyan('username')} ← Update your username to ${chalk.cyan('username')}
-/room ${chalk.green('name')} ← TODO: Create and automatically join a room called ${chalk.green(
-  'name'
-)}
+/room ${chalk.green('name')} ← Create and automatically join a room called ${chalk.green('name')}
 `;
 
 module.exports = instructions;
