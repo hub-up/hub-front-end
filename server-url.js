48643e11954e2a2ca14391e73ab50e7290fcdfc8
Joseph <z@charmed.tech>
2019-04-16 15:48:20 -0700

    Fix alex's horrible hardcoding

--- server-url.js
+++ server-url.js
@@ -1,5 +1,4 @@
-// let SERVER_URL = 'https://afternoon-eyrie-23717.herokuapp.com';
-let SERVER_URL = 'http://localhost:3000';
+let SERVER_URL = 'https://afternoon-eyrie-23717.herokuapp.com';
 
 if (process.env.NODE_ENV === 'development') {
   SERVER_URL = 'http://127.0.0.1:3000';
